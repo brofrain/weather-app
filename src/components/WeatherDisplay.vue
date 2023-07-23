b646110301e1f50779e911a6d5a83a90383b1604
Joanna Panimasz <pani.joanna93@gmail.com>
2023-07-23 15:33:38 +0200

    create WeatherDisplay component

---
+++ src/components/WeatherDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div
+      class="bg-sectionImage bg-no-repeat bg-cover bg-top bg-fixed w-4/5 h-80 rounded-lg p-px flex justify-between items-end mx-auto"
+    >
+      <div class="m-5">
+        <p class="font-light text-5xl">25°C</p>
+        <p class="font-extralight text-2xl">Warsaw, PL</p>
+      </div>
+      <div class="m-5 flex flex-col items-end">
+        <div class="flex items-center">
+          <img class="w-8 m-1" src="/src/assets/clock.svg" alt="Clock Icon" />
+          <p class="font-light text-5xl">2:34 PM</p>
+        </div>
+        <p class="font-extralight text-2xl">clear sky, Monday</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script></script>
+
+<style></style>
