b3bee2c00dafa87323a15101725d382f101ac24b
Joanna Panimasz <pani.joanna93@gmail.com>
2023-07-27 12:45:29 +0200

    add functionality to the WeatherDisplay

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import SearchSidebar from "./components/SearchSidebar.vue";
 import WeatherDisplay from "./components/WeatherDisplay.vue";
 </script>
 
 <template>
-  <SearchSidebar />
   <WeatherDisplay />
 </template>
 
-<style scoped></style>
+<style></style>
