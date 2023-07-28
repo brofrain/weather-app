8b850b2a793958778f53971616862f5fc847f999
Joanna Panimasz <pani.joanna93@gmail.com>
2023-07-28 16:09:19 +0200

    add error and loading state to the WeatherDisplay

--- src/components/WeatherDisplay.vue
+++ src/components/WeatherDisplay.vue
@@ -4,6 +4,8 @@ import { ref } from "vue";
 
 const weatherData = ref(null);
 const location = ref("");
+const loading = ref(false);
+const error = ref("");
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -12,6 +14,9 @@ const vFocus = {
 };
 
 const fetchWeatherData = async () => {
+  loading.value = true;
+  error.value = "";
+
   try {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather`,
@@ -24,9 +29,12 @@ const fetchWeatherData = async () => {
       }
     );
     weatherData.value = response.data;
-  } catch (error) {
-    console.log(error.response);
+  } catch (e) {
+    error.value = e;
   }
+  setTimeout(() => {
+    loading.value = false;
+  }, 500);
 };
 </script>
 
@@ -45,7 +53,25 @@ const fetchWeatherData = async () => {
         placeholder="Enter location..."
       />
       <div
-        v-if="weatherData"
+        v-if="loading"
+        class="m-5 p-10 flex flex-col items-center justify-between max-md:m-5 max-md:items-center"
+      >
+        <img
+          class="w-30 rounded-full animate-spin"
+          src="../assets/spinner.svg"
+          alt="Spinner Icon"
+        />
+        <p class="text-3xl text-slate-800">Loading...</p>
+      </div>
+      <div
+        v-else-if="error"
+        class="m-5 flex flex-col items-center justify-between max-md:m-5 max-md:items-center p-5"
+      >
+        <img class="w-12 m-2" src="../assets/warning.svg" alt="Warning Icon" />
+        <p class="text-neutral-600 font-bold text-xl">{{ error }}</p>
+      </div>
+      <div
+        v-else-if="weatherData"
         class="m-5 p-10 flex flex-col items-center justify-between max-md:m-5 max-md:items-center"
       >
         <p class="text-7xl pb-5 max-md:text-5xl">
